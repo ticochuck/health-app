606f1b2422e2c2ec480188915d9d819592eb829c
ticochuck <chucklivilla@gmail.com>
2021-03-04 11:09:43 -0800

    started model

--- weights/models.py
+++ weights/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    weight = models.CharField(max_length=6)
+    date_posted = models.DateTimeField(default=timezone.now)
+    notes = models.TextField(blank=True)
+
+
+    def __str__(self):
+        return f'{self.user}, {self.weight}'
+
+
+
--- weights/views.py
+++ weights/views.py
@@ -1,18 +1,24 @@
 from django.shortcuts import render
+from .models import Post
 
 data = [
     {
         'user': 'Chuck',
         'weight': '170',
+        'date_posted': '11/11/2020',
+        'notes': 'Hello from Chuck',
     },
     {
         'user': 'Laura',
         'weight': '140',
+        'date_posted': '10/1/2019',
+        'notes': 'Hello from Lau'
     }
 ]
 def home(request):
     context = {
-        'data': data
+        # 'data': data
+        'data': Post.objects.all()
     }
     return render(request, 'weights/home.html', context)
 
