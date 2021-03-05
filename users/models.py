1f527e6aaa9dd7065bd5805d66444f60089f64d6
ticochuck <chucklivilla@gmail.com>
2021-03-05 15:01:15 -0800

    change img size

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from PIL import Image
 
 
 class Profile(models.Model):
@@ -11,4 +12,14 @@ class Profile(models.Model):
         return f'{self.user.username} Profile'
     
 
+    def save(self):
+        super().save()
+
+
+        img = Image.open(self.image.path)
+
+        if img.height >00 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
 
