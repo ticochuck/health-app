0f841e47d90b608f0f589f07952bfc6fbed3e552
ticochuck <chucklivilla@gmail.com>
2021-03-18 17:07:57 -0700

    added pagination

--- weights/urls.py
+++ weights/urls.py
@@ -1,18 +1,14 @@
 from django.urls import path
 
-from .views import (
-    PostCreateView, 
-    PostDetailView, 
-    PostListView,
-    PostUpdateView,
-    PostDeleteView
-)
+from .views import (PostCreateView, PostDeleteView, PostDetailView,
+                    PostListView, PostUpdateView, UserPostListView)
 
 from.views import home, about
 
 
 urlpatterns = [
     path('', PostListView.as_view(), name='home'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-post'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
