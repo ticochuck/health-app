c70b3b4f85d263c7f306e430a27c637e14e4ae4f
ticochuck <chucklivilla@gmail.com>
2021-03-19 12:29:11 -0700

    working on email sender bug

--- health_project/settings.py
+++ health_project/settings.py
@@ -143,4 +143,10 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'home'
 LOGIN_URL = 'login'
 
-
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+ENAIL_HOST_USER = 'clemailserver'
+EMAIL_HOST_PASSWORD = 'bnogdmbytcenfbtc'
+DEFAULT_FROM_EMAIL = 'EMAIL_HOST_USER'
